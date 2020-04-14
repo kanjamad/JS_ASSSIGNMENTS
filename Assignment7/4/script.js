d250135b66aa4b6c496cccb998a29baa364200bc
kanjamad bosel <kanjamadpor@gmail.com>
2020-04-13 21:50:31 -0700

    finish ass7.4

---
+++ Assignment7/4/script.js
@@ -0,0 +1,14 @@
+
+
+const replaceImg = () => {
+        document.getElementById("rep").setAttribute("src","./img/2.jpg")
+} 
+
+
+const hideImg = () => {
+        document.getElementById("hide").style.display =  "none";
+}
+
+const showImg = () => {
+        document.getElementById('show').style.display = 'block';
+}
