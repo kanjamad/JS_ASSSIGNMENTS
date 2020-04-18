4d73673de3fff0d6821c8a248597bbc05dd16eeb
kanjamad bosel <kanjamadpor@gmail.com>
2020-04-18 13:44:43 -0700

    finish Ass 7(2) and 7(3)DOM and Event

---
+++ Assignment7/DOM and Events/2/script.js
@@ -0,0 +1,8 @@
+
+// alert("welcome to my blog ");
+
+// ----or other way -----
+
+window.onload = function() {
+    alert("welcome to my blog");
+}
---
+++ Assignment7/DOM and Events/3/script.js
@@ -0,0 +1,10 @@
+
+
+const hideElement = () => {
+    let x = document.getElementById("hideAll");
+    if (x.style.display === "none"){
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
